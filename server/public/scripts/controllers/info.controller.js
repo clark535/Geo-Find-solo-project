3fd16e8947530795a2855973d9276eda2ee1228b
David Clark <primecomp@Davids-MacBook-Pro.local>
2017-12-29 09:45:22 -0600

    drop down works

--- server/public/scripts/controllers/info.controller.js
+++ server/public/scripts/controllers/info.controller.js
@@ -1,5 +1,12 @@
-myApp.controller('InfoController', function(UserService) {
+myApp.controller('InfoController', function(UserService, MapService) {
   console.log('InfoController created');
   var vm = this;
   vm.userService = UserService;
+  vm.countries = MapService.countries;  
+
+  //Filestack controller
+  vm.uploadFlag = UserService.uploadFlag;
+  vm.uploadImage1 = UserService.uploadImage1;
+  vm.uploadImage2 = UserService.uploadImage2;
+  vm.uploadVideo = UserService.uploadVideo;
 });
