2cc89bff215d85d31c580f12fe2a2fbbb399cee5
David Clark <primecomp@Davids-MacBook-Pro.local>
2018-01-15 14:58:29 -0600

    Added video perview to teacher view

--- server/public/scripts/controllers/map.controller.js
+++ server/public/scripts/controllers/map.controller.js
@@ -26,6 +26,9 @@ myApp.controller('MapController', function (MapService) {
         var voices = window.speechSynthesis.getVoices();
         msg.voice = voices[25]; // Note: voices are unpredictable at the moment, still experimental tech. 7 is the clearest male, 17 the clearest female, 25 is good
         msg.voiceURI = 'native';
+        msg.volume = 2; // 0 to 1
+        msg.rate = .8; // 0.1 to 10
+        msg.pitch = 1; //0 to 2       
         msg.lang = 'en-US';
         window.speechSynthesis.speak(msg);
         console.log('map zoom is', self.mapZoom);       
